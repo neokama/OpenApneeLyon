db5fa511ddf162b1cda945c36698c13b540139cb
dolleo <leo.dol@orange.fr>
2017-03-31 13:42:44 +0200

    menu
    modifications du menu pour convenir à la liste des commandes du drive

--- src/menu/menu.go
+++ src/menu/menu.go
@@ -239,32 +239,99 @@ import "flag"
 import "fmt"
 
 
-func Registration(){
-	fmt.Println("importation des participants dans la base de donnees")
-}
-
 func Parsage(){
-	choix1 := flag.String("choix1","help") // On crée ici un flag => lorsqu'on lance le programme on peut ajouter une option 
-	// pour choisir quelle fonctionnalité utiliser. Dans ce cas, le choix par défaut pour la valeur du flag est quit et "a String" n'est 
+	c := flag.String("c","deff", "a String") // On crée ici un flag => lorsqu'on lance le programme on peut ajouter une option 
+	// pour choisir quelle fonctionnalité utiliser. Dans ce cas, le choix par défaut pour la valeur du flag est help et "a String" n'est 
 	// qu'une description du flag.
-	choix2 := flag.String("choix2", "help", "a String")
+	
+	e := flag.String("e", "deff", "a String")
+	bdd := flag.String("bdd", "deff", "a String")
+	r := flag.String("r", "deff","a String")
+	
 	flag.Parse()  // permet d'initialiser le pointeur vers le flag.
-	if *choix1 == "comp"{
-		if *choix2 == "reg"{
-			Registration()
-		} else {
-			fmt.Println("Bienvenu dans l'aide ! \n")
-			fmt.Println("Vous vous trouvez actuellement dans le menu de gestion des participants.\n")
-			fmt.Println("Vous pouvez importer des participants dans la base de donnees de l'application en saisissant -choix2=reg apres votre derniere commande.")
-		}
-	} else if *choix1 == "team" {
-		fmt.Println("Vous etes dans le menu de gestion des equipes")
+	
+	if *c == "add"{
+		fmt.Println("le programme demande alors les nom, prénom, numéro, équipe etc… du compétiteur à ajouter.")
+	} else if *c == "rmv" {
+		fmt.Println("Le programme demande alors le nom et prénom du compétiteur à supprimer et demande confirmation après avoir affiché les données du compétiteur.")
+	} else if *c == "modify" {
+		fmt.Println("Le programme demande nom et prénom du compétiteur, puis la/les colonne(s) à modifier.")
+	} else if *c == "search" {
+		fmt.Println(" Le programme demande par quel élément la recherche se fait (nom, prénom, numéro…) puis le/les mot(s)-clé(s) de la recherche.")
+	} else if *c == "genbad" {
+		fmt.Println("Sélectionne un fichier csv (compétiteurs, bénévoles…) et édite les badges correspondants sous format pdf.")
+	} else if *c == "help" {
+		fmt.Println("Vous pouvez ajouter un competiteur en tapant -c=add apres votre derniere commande\n")
+		fmt.Println("Vous pouvez supprimer un competiteur en tapant -c=rmv apres votre derniere commande\n")
+		fmt.Println("Vous pouvez modifier un competiteur en tapant -c=modify apres votre derniere commande\n")
+		fmt.Println("Vous pouvez rechercher un compétiteur en tapant -c=search apres votre derniere commande\n")
+		fmt.Println("Vous pouvez generer des badges en tapant -c=genbad apres votre derniere commande\n")
 	} else {
-		fmt.Println("Bienvenu dans l'aide ! \n")
-		fmt.Println("Vous vous trouvez actuellement dans le menu principal. \n")
-		fmt.Println("Vous pouvez acceder au menu de gestion des participants en tapant -choix1=comp apres votre derniere commande.\n")
-		fmt.Println("Vous pouvez acceder au menu de gestion des equipes en tapant -choix1=team apres votre derniere commande.\n")
+	
+		if *e == "add" {
+			fmt.Println("Ajout d'une equipe a la bdd")
+		} else if *e == "rmv" {
+			fmt.Println("suppression d'une equipe de la bdd")
+		} else if *e == "modify" {
+			fmt.Println("Modification d'une equipe de la bdd")
+		} else if *e == "search" {
+			fmt.Println("Recherche d'une equipe dans la bdd")
+		} else if *e == "check" {
+			fmt.Println("Permet de demander la verification de la validite d’une equipe")
+		} else if *e == "help" {
+			fmt.Println("Vous pouvez ajouter une equipe en tapant -e=add apres votre derniere commande\n")
+			fmt.Println("Vous pouvez supprimer une equipe en tapant -e=rmv apres votre derniere commande\n")
+			fmt.Println("Vous pouvez modifier une equipe en tapant -e=modify apres votre derniere commande\n")
+			fmt.Println("Vous pouvez rechercher une equipe en tapant -e=search apres votre derniere commande\n")
+			fmt.Println("Vous pouvez verifier la validite dune equipe en tapant -e=check apres votre derniere commande\n")
+		} else {
+		
+			if *bdd == "reset" {
+				fmt.Println("Remise a zero de la bdd")
+			} else if *bdd == "save" {
+				fmt.Println("Copie les fichiers csv contenant les compétiteurs, les scores ainsi que le planning avec timestamp, les place dans un dossier save afin d’avoir des sauvegardes.")
+			} else if *bdd == "import" {
+				fmt.Println("Demande le chemin d’accès au fichier csv contenant les compétiteurs")
+			} else if *bdd == "genpla" {
+				fmt.Println("Lance la vérification puis la génération du planning, ainsi que des fiches épreuves")
+			} else if *bdd == "help" {
+				fmt.Println("Vous pouvez remettre la bdd a zero en tapant -bdd=reset apres votre derniere commande\n")
+				fmt.Println("Vous pouvez creer des sauvegardes en tapant -bdd=save apres votre derniere commande\n")
+				fmt.Println("Vous pouvez importer les donnees dun fichier csv en tapant -bdd=import apres votre derniere commande\n")
+				fmt.Println("Vous pouvez generer des plannings en tapant -bdd=genpla apres votre derniere commande\n")
+			} else {
+			
+				if *r == "add" {
+					fmt.Println("Demande le nom et la performance du compétiteur ainsi que le nom de l’épreuve sur laquelle on veut ajouter des résultats afin de l’ajouter au tableau des scores")
+				} else if *r == "import" {
+					fmt.Println("Importe un fichier .csv de résultats d’une épreuve")
+				} else if *r == "modify" {
+					fmt.Println("Modifie le score d’un compétiteur")
+				} else if *r == "print" {
+					fmt.Println("Génère une feuille de résultats d’une épreuve lorsqu’elle est terminée")
+				} else if *r == "genclass" {
+					fmt.Println("Lance la génération du classement final une fois que les épreuves sont terminées")
+				} else if *r == "rmv" {
+					fmt.Println("Supprime le score d’un participant à une épreuve")
+				} else if *r == "help" {
+					fmt.Println("Vous pouvez inscrire la performance dun competiteur en tapant -r=add apres votre derniere commande\n")
+					fmt.Println("Vous pouvez importer un fichier csv de resultats en tapant -r=import apres votre derniere commande\n")
+					fmt.Println("Vous pouvez modifier le score dun competiteur en tapant -r=modify apres votre derniere commande\n")
+					fmt.Println("Vous pouvez generer une feuille de resultat pour une epreuve donnee en tapant -r=print apres votre derniere commande\n")
+					fmt.Println("Vous pouvez generer le classement final en tapant -r=genclass apres votre derniere commande\n")
+					fmt.Println("Vous pouvez supprimer le score dun participant a une epreuve en tapant -r=rmv apres votre derniere commande\n")
+				} else {
+					fmt.Println("Bienvenu dans l'aide ! \n")
+					fmt.Println("Vous pouvez acceder aux options de gestion dun participant en tapant -c=help apres votre derniere commande\n")
+					fmt.Println("Vous pouvez acceder aux options de gestion dune equipe en tapant -e=help apres votre derniere commande\n")
+					fmt.Println("Vous pouvez acceder aux options dinteraction avec la bdd en tapant -bdd=help apres votre derniere commande\n")
+					fmt.Println("Vous pouvez acceder aux options de gestion des resultats en tapant -r=help apres votre derniere commande\n")
+				}
+			}
+		}
 	}
+	
+	
 	
 }
 
