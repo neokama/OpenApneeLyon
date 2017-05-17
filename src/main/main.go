c36ca9c981f2d114f9526343be233d3ab18cfb05
Arnaud <arno969@gmail.com>
2017-05-17 15:28:47 +0200

    Modif main

--- src/main/main.go
+++ src/main/main.go
@@ -7,29 +7,29 @@ import (
 	
 	
 func main() {
-	fmt.Println("Début: \n")
-	//Parsage()
+	// fmt.Println("Début: \n")
+	Parsage()
 
 	
 	// %%%%%%%%%% TEST CLASSEMENT %%%%%%%%
-	base := newBdd("database/OpenApneeLyon")
-	base.reset()
-	base.importCompetiteur()
-	base.importResultat()
-	fmt.Println(" classement ->\n")
-	//base.displayClassement()
-	base.exportClassement("spd")
-	//base.displayClassement()
-	base.exportClassement("dnf")
-	base.exportClassement("dwf")
-	base.exportClassement("1650")
-	base.exportClassement("sta")
-	base.displayClassement()
-	//base.displayCompetiteur()
-	fmt.Println("\n")
-	fmt.Println("Equipe -> ")
-	base.exportClassementEquipe()
-	base.displayEquipe()
+	// base := newBdd("database/OpenApneeLyon")
+	// base.reset()
+	// base.importCompetiteur()
+	// base.importResultat()
+	// fmt.Println(" classement ->\n")
+	// base.displayClassement()
+	// base.exportClassement("spd")
+	// base.displayClassement()
+	// base.exportClassement("dnf")
+	// base.exportClassement("dwf")
+	// base.exportClassement("1650")
+	// base.exportClassement("sta")
+	// base.displayClassement()
+	// base.displayCompetiteur()
+	// fmt.Println("\n")
+	// fmt.Println("Equipe -> ")
+	// base.exportClassementEquipe()
+	// base.displayEquipe()
 
 	
 }
