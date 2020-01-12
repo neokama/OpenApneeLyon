6ab1c92d33713e136db98d5d3b1efe1e3954bbd8
neokama <mathieu.carchi+github@gmail.com>
2020-01-12 22:34:51 +0100

    remove GOROOT setup from build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export GOPATH=$(pwd)
-export GOROOT=$(find /usr/lib -type d -name "go-*"  | tail -n 1)
+#export GOROOT=$(find /usr/lib -type d -name "go-*"  | tail -n 1)
 
 go install oaem
